63a9ab533bde8e2478fef8537c9d3c813aa57e73
Aline Rosa <alinerosa_s@hotmail.com>
2022-12-01 22:36:13 -0300

    Continuando com projeto

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from app.models import Person
+
+
+# Aqui é onde criamos os campos do form
+class PersonForm(ModelForm):
+    class Meta:
+        model = Person
+        fields = ['modelo', 'marca', 'ano']
--- app/views.py
+++ app/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from app.forms import PersonForm
+from app.models import Person
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db'] = Person.objects.all()
+    return render(request, 'index.html', data)
+
 
 def form(request):
-    return render(request, 'form.html')
+    data = {}
+    data['form'] = PersonForm()
+    return render(request, 'form.html', data)
+
+
+def create(request):
+    form = PersonForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Person.objects.get(pk=pk)
+    return render(request, 'view.html', data)
